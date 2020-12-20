a58cbde891129e0344fd2b4389afbae6022e21dc
Nitish Mittal <nitish7134@gmail.com>
2020-12-21 04:23:12 +0530

    Working Mic

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-import  {GLTFLoader}  from 'three/examples/jsm/loaders/GLTFLoader.js';
-
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.123.0/build/three.module.js';
 const loader = new GLTFLoader();
 
+const texture = new THREE.TextureLoader().load( '/microphone/textures/01___Default_baseColor.jpeg' );
+const ematerial = new THREE.MeshBasicMaterial( { map: texture } );
+
 loader.load( './microphone/scene.gltf', function ( gltf ) {
-
-	scene.add( gltf.scene );
-
+    const model = gltf.scene;
+	scene.add(  model );
 }, undefined, function ( error ) {
-
-	console.error( error );
-
+    
+    console.error( error );
+    
 } );
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(0xffffff)
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
+//scene.add(cube);
 camera.position.z = 5;
 
 const animate = function () {
