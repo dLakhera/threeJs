acab1572d1b41ddbebd2f0a406c30bbc1fdfc440
droidLakhera <dlakhera02@gmail.com>
2020-12-21 20:52:28 +0530

    Added a soldier to scene

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three/examples/jsm/loaders/GLTFLoader.js';
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.123.0/build/three.module.js';
-// import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three-orbitcontrols@2.110.3/OrbitControls.min.js";
 import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
@@ -9,10 +8,13 @@ const loader = new GLTFLoader();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 
-renderer.setClearColor(0xff0000);
+camera.position.z =-5;
+
+renderer.setClearColor(0xffffff);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.outputEncoding = THREE.sRGBEncoding;
 document.body.appendChild(renderer.domElement);
-camera.position.z = 2;
+// camera.position.z = 2;
 
 
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
@@ -31,31 +33,16 @@ dirLight.shadow.camera.far = 40;
 scene.add(dirLight);
 
 //Microphone
-loader.load('./microphone/scene.gltf', function (gltf) {
+loader.load('./barbarian/scene.gltf', function (gltf) {
     const model = gltf.scene;
-    const tl = new THREE.TextureLoader();
-    const texture = tl.load('./microphone/textures/01___Default_metallicRoughness.png')
 
-
-
-    // model.metalnessMap = texture;
-    // const texture = textLoader.load('./microphone/textures/01___Default_normal.jpeg')
-    // const mtrl = new THREE.MeshBasicMaterial({ map: texture });
-    // const basetexture = new THREE.TextureLoader().load('/microphone/textures/01___Default_baseColor.jpeg');
-    // const metalicRoughnesstexture = new THREE.TextureLoader().load('/microphone/textures/01___Default_metallicRoughness.png');
-    // const normaltexture = new THREE.TextureLoader().load('/microphone/textures/01___Default_normal.jpeg');
-    // const micMaterial = new THREE.MeshStandardMaterial({ color: 'purple' });
-    // micMaterial.map = basetexture;
-    // micMaterial.metalnessMap = metalicRoughnesstexture;
-    // micMaterial.normalMap = normaltexture;
-    // micMaterial.color = "#0xff0000";
-    // model.material = micMaterial;
-
+    model.scale.multiplyScalar(1/10);
+    // model.position.y = -20;
 
     scene.add(model);
     const animate = function () {
         requestAnimationFrame(animate);
-    
+        
         // model.rotation.x += 0.01;
         model.rotation.y += 0.01;
     
@@ -71,7 +58,7 @@ loader.load('./microphone/scene.gltf', function (gltf) {
 
 
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.addEventListener('change', render); // use if there is no animation loop
+// controls.addEventListener('change', render); // use if there is no animation loop
 controls.minDistance = 2;
 controls.maxDistance = 10;
 controls.target.set(0, 0, - 0.2);
